b89eded82217e70bbe1a43a7da30a5a88d960975
Timothy Hobbs <timothy@hobbs.cz>
2023-05-15 13:54:58 +0200

    Try without RAID

--- nixos/disko.nix
+++ nixos/disko.nix
@@ -1,110 +1,41 @@
-{ disks ? [ "/dev/nvme0n1" "/dev/nvme1n1" ], ... }: {
-    disk = {
-      one = {
-        type = "disk";
-        device = builtins.elemAt disks 0;
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "boot";
-              start = "0";
-              end = "1M";
-              part-type = "primary";
-              flags = [ "bios_grub" ];
-            }
-            {
-              name = "ESP";
-              start = "1MiB";
-              end = "128MiB";
-              fs-type = "fat32";
-              bootable = true;
-              content = {
-                type = "mdraid";
-                name = "boot";
-              };
-            }
-            {
-              name = "mdadm";
-              start = "128MiB";
-              end = "100%";
-              content = {
-                type = "mdraid";
-                name = "raid1";
-              };
-            }
-          ];
-        };
-      };
-      two = {
-        type = "disk";
-        device = builtins.elemAt disks 1;
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "boot";
-              start = "0";
-              end = "1M";
-              part-type = "primary";
-              flags = [ "bios_grub" ];
-            }
-            {
-              name = "ESP";
-              start = "1MiB";
-              end = "128MiB";
-              fs-type = "fat32";
-              bootable = true;
-              content = {
-                type = "mdraid";
-                name = "boot";
-              };
-            }
-            {
-              name = "mdadm";
-              start = "128MiB";
-              end = "100%";
-              content = {
-                type = "mdraid";
-                name = "raid1";
-              };
-            }
-          ];
-        };
+{ disks ? [ "/dev/nvme0n1" ], ... }: {
+  disk = {
+    main = {
+      type = "disk";
+      device = builtins.elemAt disks 0;
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "boot";
+            start = "0";
+            end = "1M";
+            flags = [ "bios_grub" ];
+          }
+          {
+            name = "ESP";
+            start = "1M";
+            end = "512M";
+            bootable = true;
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          }
+          {
+            name = "root";
+            start = "512M";
+            end = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          }
+        ];
       };
     };
-    mdadm = {
-      boot = {
-        type = "mdadm";
-        level = 1;
-        metadata = "1.0";
-        content = {
-          type = "filesystem";
-          format = "vfat";
-          mountpoint = "/boot";
-        };
-      };
-      raid1 = {
-        type = "mdadm";
-        level = 1;
-        content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            {
-              name = "primary";
-              start = "1MiB";
-              end = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            }
-          ];
-        };
-      };
-    };
+  };
 }
